04d736761a65fcb3d1be5febe2761c6ae6ae335c
cauanMorais <cauandejesus329@gmail.com>
2025-05-04 21:22:34 -0300

    adicionando mecanica de fps e input do teclado

--- astro-forest/src/main/java/com/astroforest/GamePanel.java
+++ astro-forest/src/main/java/com/astroforest/GamePanel.java
@@ -20,13 +20,24 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    // Frames Per Second
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    //Set Player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 5;
 
     public GamePanel () {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread () {
@@ -38,15 +49,55 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
 
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        long drawCount = 0;
+
         while (gameThread != null ) {
             
-            // System.out.println("Game loop");
+            currentTime = System.nanoTime();
 
-            // UPDATE: Update Information such as character positions
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
 
-            // DRAW: draw the screen with the updated information 
+            if(delta >= 1) {
+                // UPDATE: Update Information such as character positions
+                update();
+                // DRAW: draw the screen with the updated information 
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if(timer >= 1000000000) {
+                System.out.println("FPS");
+            }
         }
 
+    }
+
+    public void update () {
+
+        if(keyH.upPressed == true) {
+            playerY -= playerSpeed;
+            System.out.println ("Cima");
+        } 
+        if(keyH.downPressed == true) {
+            playerY += playerSpeed;
+            System.out.println ("Baixo");
+        }
+        if(keyH.leftPressed == true) {
+            playerX -= playerSpeed;
+            System.out.println ("Esquerda");
+        }
+        if(keyH.rightPressed == true) {
+            playerX += playerSpeed;
+            System.out.println ("Direita");
+        }
     }
 
     public void paintComponent (Graphics g) {
@@ -57,7 +108,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         g2.setColor (Color.white);
         
-        g2.fillRect (100,100,tileSize, tileSize);
+        g2.fillRect (playerX,playerY,tileSize, tileSize);
     }
     
 }
--- astro-forest/src/main/java/com/astroforest/KeyHandler.java
+++ astro-forest/src/main/java/com/astroforest/KeyHandler.java
@@ -10,13 +10,39 @@ public class KeyHandler implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         
-        
+        int code = e.getKeyCode();
+
+        if(code == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+        if(code == KeyEvent.VK_S) {
+            downPressed = true;
+        }
+        if(code == KeyEvent.VK_A) {
+            leftPressed = true;
+        }
+        if(code == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         
-        throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+        int code = e.getKeyCode();
+
+        if(code == KeyEvent.VK_W) {
+            upPressed = false;
+        }
+        if(code == KeyEvent.VK_S) {
+            downPressed = false;
+        }
+        if(code == KeyEvent.VK_A) {
+            leftPressed = false;
+        }
+        if(code == KeyEvent.VK_D) {
+            rightPressed = false;
+        }
     }
 
     @Override
